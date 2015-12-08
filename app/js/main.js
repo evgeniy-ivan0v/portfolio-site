c46236f0de67d1a1b3b73d36f6174cfe17a10907
evgeniy-ivan0v <ivanov.j.1989@gmail.com>
2015-12-08 16:48:17 +0300

    add placeholder for ie8

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+function formChek () {
+	this.elem = elem = document.getElementsByTagName("form");
+	console.dir(elem)
+	// elem.addEventListener('click', function () {
+
+	// 	console.log("fvfv");
+	// });
+};
+var formChek = new formChek();
